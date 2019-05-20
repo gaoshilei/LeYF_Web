1f5e53fc8b97ddf8393708c064a3ff4222acbdab
gaoshilei <goslei@163.com>
2019-05-20 19:46:08 +0800

    动画优化

--- src/Layout/AppLayout/AppLayout.vue
+++ src/Layout/AppLayout/AppLayout.vue
@@ -29,4 +29,106 @@
     }
   }
 
+  @-webkit-keyframes unpinned {
+    0% {
+      -webkit-transform: translateY(0)
+    }
+    100% {
+      -webkit-transform: translateY(50px)
+    }
+  }
+
+  @keyframes unpinned {
+    0% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0)
+    }
+    100% {
+      -webkit-transform: translateY(50px);
+      transform: translateY(50px)
+    }
+  }
+
+  @-webkit-keyframes pinned {
+    0% {
+      -webkit-transform: translateY(50px)
+    }
+    100% {
+      -webkit-transform: translateY(0)
+    }
+  }
+
+  @keyframes pinned {
+    0% {
+      -webkit-transform: translateY(50px);
+      transform: translateY(50px)
+    }
+    100% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0)
+    }
+  }
+
+  @-webkit-keyframes slideDown {
+    0% {
+      -webkit-transform: translateY(-70px)
+    }
+    100% {
+      -webkit-transform: translateY(0)
+    }
+  }
+
+  @keyframes slideDown {
+    0% {
+      -webkit-transform: translateY(-70px);
+      transform: translateY(-70px)
+    }
+    100% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0)
+    }
+  }
+
+  @-webkit-keyframes slideUp {
+    0% {
+      -webkit-transform: translateY(0)
+    }
+    100% {
+      -webkit-transform: translateY(-70px)
+    }
+  }
+
+  @keyframes slideUp {
+    0% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0)
+    }
+    100% {
+      -webkit-transform: translateY(-70px);
+      transform: translateY(-70px)
+    }
+  }
+
+  @keyframes fade-in {
+    0% {
+      transform: translateY(20px);
+      opacity: 0
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 1
+    }
+  }
+
+  @-webkit-keyframes fade-in {
+    0% {
+      -webkit-transform: translateY(20px);
+      opacity: 0
+    }
+    100% {
+      -webkit-transform: translateY(0);
+      opacity: 1
+    }
+  }
+
 </style>
--- src/Layout/AppLayout/components/AppHeader.vue
+++ src/Layout/AppLayout/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <div class="app-header clearfix">
+    <div class="app-header clearfix" :class="headerClass">
       <span class="site-title">LeonLei的博客</span>
       <div class="navbar-menu">
         <router-link to="/archive">归档</router-link>
@@ -13,7 +13,32 @@
 
 <script>
   export default {
-    name: "AppHeader"
+    name: "AppHeader",
+    data() {
+      return {
+        offsetTop: 0,
+        headerClass: ''
+      }
+    },
+    methods: {
+      // 滚动监听  滚动触发的效果写在这里
+      handleScroll() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+        let scroll = scrollTop - this.offsetTop;
+        this.offsetTop = scrollTop;
+        if (scroll < 0) {
+          this.headerClass = ' animated headroom--not-bottom slideDown headroom--top';
+        } else {
+          this.headerClass = ' animated headroom--not-bottom headroom--not-top slideUp';
+        }
+      }
+    },
+    mounted() {
+      // 设置bar浮动阈值为 #fixedBar 至页面顶部的距离
+      this.offsetTop = document.querySelector('#header').offsetHeight;
+      // 开启滚动监听
+      window.addEventListener('scroll', this.handleScroll);
+    },
   }
 </script>
 
@@ -27,6 +52,7 @@
     line-height: 70px;
     background: #ffffff;
     box-shadow: 0 1px 5px rgba(0, 0, 0, .1);
+
     .app-header {
       font-size: 22px;
       color: #000;
@@ -34,6 +60,13 @@
       max-width: 100%;
       height: 70px;
       margin: 0 auto;
+
+      .animated {
+        -webkit-animation-duration: .5s;
+        animation-duration: .5s;
+        -webkit-animation-fill-mode: both;
+        animation-fill-mode: both
+      }
 
       .site-title {
         float: left;
